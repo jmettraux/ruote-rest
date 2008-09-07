259029c2fb530de351494242a6a38a46c458461d
John Mettraux <jmettraux@gmail.com>
2008-09-07 13:10:54 +0900

    added -p / -e to lib/start.rb

--- lib/start.rb
+++ lib/start.rb
@@ -1,4 +1,6 @@
 
+require 'optparse'
+
 dir = File.dirname(__FILE__)
 
 $:.unshift "#{dir}"
@@ -9,11 +11,39 @@ $:.unshift "#{dir}/../vendor" # if any
 $:.unshift "~/ruote/lib"
 $:.unshift "~/rufus/rufus-sixjo/lib"
 
+#
+# parse options
+
+port = 4567
+
+opts = OptionParser.new
+
+opts.banner = 'Usage: ruby lib/start.rb [options]'
+opts.separator('')
+opts.separator('options:')
+
+opts.on('-p', '--port {port}', 'which port to listen to') do |v|
+  port = v.to_i
+end
+opts.on('-e', '--env {env}', 'which env ? development/production') do |v|
+  ENV['ruote.environment'] = v
+end
+opts.on('-h', '--help', 'display this help content') do
+  puts
+  puts opts.to_s
+  puts
+  exit 0
+end
+
+opts_rest = opts.parse(ARGV)
+
+
+#
+# run ruote-rest
 
 require 'rubygems'
 
 load 'ruote_rest.rb'
-
 
 b = Rack::Builder.new do
 
@@ -21,8 +51,6 @@ b = Rack::Builder.new do
   use Rack::ShowExceptions
   run $app
 end
-
-port = 4567 # TODO : optparse me
 
 puts ".. [#{Time.now}] ruote-rest listening on port #{port}"
 
