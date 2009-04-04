0a10633b8eff8d13bbf1123b55c760412117eb07
John Mettraux <jmettraux@gmail.com>
2009-04-04 18:22:16 +0900

    rack app startup dev/test/prod cleaned up

--- conf/db.rb
+++ conf/db.rb
@@ -1,29 +1,28 @@
 
-#gem 'activerecord'
 require 'active_record'
 
 module RuoteRest
 
-configure do
+  configure do
 
-  configuration = YAML.load_file(
-    File.join(RUOTE_BASE_DIR, 'conf', 'database.yaml')
-  )[application.environment]
+    configuration = YAML.load_file(
+      File.join(RUOTE_BASE_DIR, 'conf', 'database.yaml')
+    )[application.environment]
 
-  raise(ArgumentError.new(
-    "No database configuration for #{application.environment} environment!")
-  ) if configuration.nil?
+    raise(ArgumentError.new(
+      "No database configuration for #{application.environment} environment!")
+    ) if configuration.nil?
 
-  ActiveRecord::Base.establish_connection(
-    :adapter => configuration['adapter'],
-    :database => configuration['database'],
-    :username => configuration['username'],
-    :password => configuration['password'],
-    :host => configuration['host'],
-    :encoding => configuration['encoding'],
-    :pool => 30
-  )
-end
+    ActiveRecord::Base.establish_connection(
+      :adapter => configuration['adapter'],
+      :database => configuration['database'],
+      :username => configuration['username'],
+      :password => configuration['password'],
+      :host => configuration['host'],
+      :encoding => configuration['encoding'],
+      :pool => 30
+    )
+  end
 
 end
 
